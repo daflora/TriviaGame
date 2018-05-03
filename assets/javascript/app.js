73d43f7265326ed67f3e0ed58495d2cc4649f6f4
Maya Angelou <MayaAngelou@poets.com>
2018-05-03 00:34:54 -0700

    added countdown timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,11 +7,44 @@ var images = [
   "assets/images/saturn.jpg"
 ];
 
+// PseudoCode:
+//
+//  "click" starts game.
+//      can't get it to hold off from starting
+// Function:
+//  for(i=0;length;i++){
+//    start/reset timer
+//    show question[i]
+//    SHOW answers[i] (1 per button)
+//    logic tree:
+        // if (right answer):
+        //   score++;
+        //   next
+        // else if (wrong answer):
+        //   show answer(2500);
+        //   next
+        // if (time runs out):
+        //   show watchpicture(2500);
+        //   next}
+//    };
+//  IDS:  #question
+//        #timer
+//        #tracker
+//        #btnA
+//        #btnB
+//        #btnC
+//        #btnD
 // Variable showImage will hold the setInterval when we start the slideshow
 var showImage;
 // Count will keep track of the index of the currently displaying picture.
 var count = 0;
 
+$("#btnD").on("click", runTrivia());
+
+function runTrivia(){
+  $("#question").html("<h2>WE START</h2>");
+
+}
 // TODO: Use jQuery to run "startSlideshow" when we click the "start" button.
 // $("#start").on("click", startSlideshow);
 // TODO: Use jQuery to run "stopSlideshow" when we click the "stop" button.
@@ -22,34 +55,29 @@ var count = 0;
 function displayImage() {
   $("#image-holder").html("<img src=" + images[count] + " width='400px'>");
 }
+var number = 10;
+var intervalId;
 
-function nextImage() {
+function run() {
+  clearInterval(intervalId);
+  intervalId = setInterval(decrement, 1000);
+}
 
-  // TODO: Increment the count by 1.
-  count++;
-
-  // TODO: Show the loading gif in the "image-holder" div.
-  $("#image-holder").html("<img src='images/loading.gif' width='400px'>");
-
-
-  // TODO: Use a setTimeout to run displayImage after 1 second.
-  setTimeout(displayImage, 1000);
-
-  // TODO: If the count is the same as the length of the image array, reset the count to 0.
-  if (count >= images.length) {
-    count = 0;
+function decrement() {
+  //  Decrease number by one.
+  number--;
+  //  Show the number in the #show-number tag.
+  $("#timer").html("<h2>" + number + "</h2>");
+  //  Once number hits zero...
+  if (number === 0) {
+    stop();
+    //  Alert the user that time is up.
+    alert("Time Up!");
   }
 }
-function startSlideshow() {
 
-  // TODO: Use showImage to hold the setInterval to run nextImage.
-  showImage = setInterval(nextImage, 3000);
+function stop() {
+  clearInterval(intervalId);
 }
-function stopSlideshow() {
-
-  // TODO: Put our clearInterval here:
-  clearInterval(showImage);
-}
-
 // This will run the display image function as soon as the page loads.
 displayImage();
