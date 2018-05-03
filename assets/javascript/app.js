1c46d53adba384ebceaaa75538e59db2e7475f87
Maya Angelou <MayaAngelou@poets.com>
2018-05-03 01:47:33 -0700

    added variable text to buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,6 +7,53 @@ var images = [
   "assets/images/saturn.jpg"
 ];
 
+var questionList = [
+  "PLACEHOLDER",
+  "This special type of black hole would allow a person to cross the event horizon without being torn apart by tidal forces.",
+  "This moon of Jupiter is the most volcanically active place in the Solar System.",
+  "This is the closest galaxy outside of our own Milky Way.",
+  "These two minor planets orbit each other, as their center of gravity lies between them.",
+  "IT HAS RINGS! WHAT IS IT????"
+];
+
+var btnAAnswers = [
+  "A. DON'T PICK ME",
+  "A. Quasar",
+  "A. Ganymede",
+  "A. Phobos and Deimos",
+  "A. Pluto and Charon",
+  "A. satURN"
+];
+
+var btnBAnswers = [
+  "B. PLACEHOLDER",
+  "B. ANY Black Hole! I saw the film, yo.",
+  "B. Io",
+  "B. Andromeda",
+  "B. Phobos and Deimos!",
+  "B. SaTuRn"
+];
+
+var btnCAnswers = [
+  "C. I MEAN IT. DO NOT DO IT.",
+  "C. Supermassive Black Hole",
+  "C. Europa",
+  "C. Alpha Centauri",
+  "C. Neptune and Uranus",
+  "C. SATurn"
+];
+
+var btnDAnswers = [
+  "D. Fool! Were you born in the Sun?",
+  "D. Microscopic Black Hole",
+  "D. Callisto",
+  "D. Magellanic Cloud",
+  "D. Titan and Triton",
+  "D. Jupiter! I mean Saturn!"
+];
+
+var answerKey = ["0","C","B","D","A","D"];
+
 // PseudoCode:
 //
 //  "click" starts game.
@@ -19,12 +66,12 @@ var images = [
 //    logic tree:
         // if (right answer):
         //   score++;
-        //   next
+        //   alert(CORRECT)
         // else if (wrong answer):
         //   show answer(2500);
-        //   next
+        //   alert(Actually...)
         // if (time runs out):
-        //   show watchpicture(2500);
+        //   show alert;
         //   next}
 //    };
 //  IDS:  #question
@@ -35,23 +82,44 @@ var images = [
 //        #btnC
 //        #btnD
 // Variable showImage will hold the setInterval when we start the slideshow
-var showImage;
-// Count will keep track of the index of the currently displaying picture.
+// var showImage;
 var count = 0;
+var score = 0;
 
-$("#btnD").on("click", runTrivia());
 
+$("#question").html("<h1>Astro Trivia!</h1><h2>The Sky! It's Full of Stars! Hooray!</h2>");
+$("#tracker").html("<small>Click any button to begin...</small>");
+
+$("#buttons").on("click",runTrivia);
+
+// Game framework handles start transition and final
 function runTrivia(){
   $("#question").html("<h2>WE START</h2>");
-
+  // setTimeout(function(){$("#question").html("<h2>WE START</h2>");},500);
+  // setTimeout(function(){$("#question").html("<h2>GET READY!</h2>");},1500);
+  runLoop();
 }
-// TODO: Use jQuery to run "startSlideshow" when we click the "start" button.
-// $("#start").on("click", startSlideshow);
-// TODO: Use jQuery to run "stopSlideshow" when we click the "stop" button.
-// $("#stop").on("click", stopSlideshow);
 
-// This function will replace display whatever image it's given
-// in the 'src' attribute of the img tag.
+// Loop runs the question/answer part
+function runLoop(){
+  for (var i = 1; i<questionList.length; i++){
+  scoreKeep();
+  $("#question").html("<h2>"+questionList[i]+"</h2>");
+  $("#btnA").html("<h3>"+btnAAnswers[i]+"</h3>");
+  $("#btnB").html("<h3>"+btnBAnswers[i]+"</h3>");
+  $("#btnC").html("<h3>"+btnCAnswers[i]+"</h3>");
+  $("#btnD").html("<h3>"+btnDAnswers[i]+"</h3>");
+  run();
+
+
+
+  }
+}
+
+function scoreKeep(){
+  $("#tracker").html("Current score: " + score + " out of " + (count));
+}
+
 function displayImage() {
   $("#image-holder").html("<img src=" + images[count] + " width='400px'>");
 }
@@ -64,15 +132,11 @@ function run() {
 }
 
 function decrement() {
-  //  Decrease number by one.
   number--;
-  //  Show the number in the #show-number tag.
   $("#timer").html("<h2>" + number + "</h2>");
-  //  Once number hits zero...
   if (number === 0) {
     stop();
-    //  Alert the user that time is up.
-    alert("Time Up!");
+    alert("Time Up! Your score is currently: " + score);
   }
 }
 
