76682f17c9545fc6ff4ce7f925f7adaa0c1e28e0
Maya Angelou <MayaAngelou@poets.com>
2018-05-03 22:39:11 -0700

    attempted to address button reg issue and timing issue

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -52,12 +52,13 @@ var btnDAnswers = [
   "D. Jupiter! I mean Saturn!"
 ];
 
-var answerKey = ["0","btnC","btnB","btnD","btnA","btnD"];
+var answerKey = ["0","C","B","D","A","D"];
 
 // PseudoCode:
 //
 //  "click" starts game.
-//      can't get it to hold off from starting
+//      can't get the timer to delay, or to reset
+//      having difficulty getting the buttons to register their events
 // Function:
 //  for(i=0;length;i++){
 //    start/reset timer
@@ -85,49 +86,90 @@ var answerKey = ["0","btnC","btnB","btnD","btnA","btnD"];
 // var showImage;
 var count = 0;
 var score = 0;
-var number = 10;
+var number = 5;
 var intervalId;
-
+var timerId;
 
 $("#question").html("<h1>Astro Trivia!</h1><h2>The Sky! It's Full of Stars! Hooray!</h2>");
 $("#tracker").html("<small>Click any button to begin...</small>");
 $("#timer").html("<h2>HELLO!</h2>");
-$("#buttons").on("click",runTrivia);
+$("#buttons").on("click",function(){
+    if(count===0){runTrivia();}
+  });
 
-// Game framework handles start transition and final
+// Game framework handles start transition, supposedly assigns buttons
 function runTrivia(){
   $("#question").html("<h2>WE START</h2>");
-  // setTimeout(function(){$("#question").html("<h2>WE START</h2>");},500);
-  // setTimeout(function(){$("#question").html("<h2>GET READY!</h2>");},1500);
-  runLoop();
+  buttonAssign();
+  setTimeout(function(){$("#question").html("<h2>GET READY!</h2>");},1500);
+  number = 10;
+  question();
 }
 
-// Loop runs the question/answer part
-function runLoop(){
-  for (var i = 1; i<questionList.length; i++){
-    scoreKeep();
-    $("#question").html("<h2>"+questionList[i]+"</h2>");
-    $("#btnA").html("<h3>"+btnAAnswers[i]+"</h3>");
-    $("#btnB").html("<h3>"+btnBAnswers[i]+"</h3>");
-    $("#btnC").html("<h3>"+btnCAnswers[i]+"</h3>");
-    $("#btnD").html("<h3>"+btnDAnswers[i]+"</h3>");
-    run();
-    displayImage(i);
-    $(".buttons").onclick = evaluator(i);
+// function runs the question/answer part
+function question(){
+  timerId = setInterval(nextQuestion(), 5000);
+  stop();
+  run();
+  // $(document).on("click",'#buttons',function(event){
+  $(document).on("click",'#buttons',function(event){
+    var answer = event.target.value;
+    // console.log(answer);
+    if (answer === answerKey[count]){
+      // stop();
+      score++;
+      intervalId = setInterval(rightAnswer(),3000);
+      setInterval(nextQuestion(), 3000);
+    }
+    else if (answer !== answerKey[count]){
+      // stop();
+      intervalId = setInterval(wrongAnswer(),3000);
+      setInterval(nextQuestion(), 3000);
+    }
+  });
+}
 
+function nextQuestion(){
+  // stop();
+  count++;
+  $("#question").html("<h3>"+ questionList[count]+"</h3>");
+  displayImage(count);
+  $("#btnA").html("<h3>"+btnAAnswers[count]+"</h3>");
+  $("#btnB").html("<h3>"+btnBAnswers[count]+"</h3>");
+  $("#btnC").html("<h3>"+btnCAnswers[count]+"</h3>");
+  $("#btnD").html("<h3>"+btnDAnswers[count]+"</h3>");
+  // run();
 
-  // var currentAnswer = $("#buttons").on("click",function(){this.btn})
-  // This might be easier with objects, as there is a bit over DRY. Gar.
-  // decision tree goes here.
+  if (count >= images.length) {
+    count = 0;
+    stop();
   }
 }
 
-function evaluator(loopIndex){
-  if (".buttons"){
-    setTimeout(function(){$("#question").html("<h2>GET READY!</h2>");},1500);
-    $("#image-holder").html("<img src=assets/images/right.jpeg width='400px'>");
-  }
-  
+function rightAnswer(){
+  score++;
+  $("#question").html("<h1>CORRECT!</h1>");
+  scoreKeep();
+  $("#image-holder").html("<img src=assets/images/right.jpeg width='400px' height='200px'>");
+}
+
+function wrongAnswer(){
+  // score++;
+  $("#question").html("<h3>Sorry! Back to the telescopes for you!</h3>");
+  scoreKeep();
+  $("#image-holder").html("<img src=assets/images/wrong.jpeg width='400px' height='200px'>");
+}
+
+// Having difficulty here. Tried to assign two different ways, but inconsistent results for both.
+function buttonAssign(){
+  $("#btnA").val("A");
+  $("#btnB").val("B");
+  $("#btnC").attr("value","C");
+  $("#btnD").attr("value","D");
+
+  $(document).on("click",'#btnB',function(event){
+    console.log(event.target.value);
+  });
 }
 
 function scoreKeep(){
@@ -135,11 +177,12 @@ function scoreKeep(){
 }
 
 function displayImage(index) {
-  $("#image-holder").html("<img src=" + images[index] + " width='400px'>");
+  $("#image-holder").html("<img src=" + images[index] + " width='400px' height='200px'>");
 }
 
-
+//  Runaway timers is an issue as well.
 function run() {
+  number=8;
   clearInterval(intervalId);
   intervalId = setInterval(decrement, 1000);
 }
@@ -147,14 +190,17 @@ function run() {
 function decrement() {
   number--;
   $("#timer").html("<h2>" + number + "</h2>");
-  if (number === 0) {
+  if (number <= 0) {
     stop();
+    number=10;
     alert("Time Up! Your score is currently: " + score);
   }
 }
 
 function stop() {
-  clearInterval(intervalId);
+
+  // clearInterval(intervalId);
+  clearInterval(timerId);
 }
 // This will run the display image function as soon as the page loads.
 displayImage(count);
